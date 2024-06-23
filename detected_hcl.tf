6a946a5c8ce8035d97ce31a974a253cf96db5d2b
onose-tsubasa <onose.tsubasa@teldevice.co.jp>
2024-06-23 14:14:03 +0900

    Delete detected_hcl.tf

--- detected_hcl.tf
+++
@@ -1,6 +0,0 @@
-resource "aws_db_instance" "database" {
-  allocated_storage = 5
-  engine            = "mysql"
-  instance_class    = "db.t2.micro"
-  username          = "admin"
-  password          = "satosatosato"
